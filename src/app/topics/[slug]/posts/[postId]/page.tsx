7e7dde95bd64ef7ec8a04f1356734564dc21438d
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-14 09:38:14 -0400

    add back link to topic page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,6 +2,8 @@
 
 import PostShow from "@/components/posts/post-show";
 import db from "@/db";
+import paths from "@/paths";
+import Link from "next/link";
 import React from "react";
 
 export default async function PostShowPage({
@@ -9,5 +11,15 @@ export default async function PostShowPage({
 }: {
   params: { postId: string; slug: string };
 }) {
-  return <PostShow postId={params.postId} />;
+  return (
+    <div className="space-y-3">
+      <Link
+        className="underline decoration-solid"
+        href={paths.topicShow(params.slug)}
+      >
+        {"<"} Go back to <span className="font-bold">{params.slug}</span>
+      </Link>
+      <PostShow postId={params.postId} />
+    </div>
+  );
 }
