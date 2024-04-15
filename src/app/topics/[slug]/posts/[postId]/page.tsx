8ca26b855a53fee873d476a5f48ad885ff51725b
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-15 08:14:44 -0400

    add custom query for comments and display

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,11 @@
 /** @format */
 
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import CommentList from "@/components/comments/comment-list";
+import CommentShow from "@/components/comments/comment-show";
 import PostShow from "@/components/posts/post-show";
 import db from "@/db";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 import paths from "@/paths";
 import Link from "next/link";
 import React from "react";
@@ -20,6 +24,8 @@ export default async function PostShowPage({
         {"<"} Go back to <span className="font-bold">{params.slug}</span>
       </Link>
       <PostShow postId={params.postId} />
+      <CommentCreateForm postId={params.postId} startOpen={true} />
+      <CommentList fetchData={() => fetchCommentsByPostId(params.postId)} />
     </div>
   );
 }
